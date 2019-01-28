32e8106d9e8844effaaac077f30a11bb3e9c55eb
s-herbert <sth.accounts@gmail.com>
2019-01-27 22:22:02 -0800

    updated server listening msg

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ const { startServer } = require('./server/server');
 const config = require('./server/config/serverConfig');
 
 startServer(config)
-  .then(() => console.log('server started!!'))
+  .then(() => console.log(`Listening for requests on ${config.port}`))
   .catch(err => console.log(`Nope, cuz ${err}`));
